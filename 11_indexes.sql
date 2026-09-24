3675bbd3a513f550c8c23983add787cc11df83da
Saloni Ade <saloniade845@gmail.com>
2026-09-24 10:02:29 +0530

    Clean up SQL by removing EXPLAIN and dropping index
    Removed unnecessary EXPLAIN query and dropped index.

--- 11_indexes.sql
+++ 11_indexes.sql
@@ -10,15 +10,6 @@ JOIN district d
     ON rr.district_id = d.district_id
 WHERE rr.actual_rainfall > 500;
 
-EXPLAIN
-SELECT
-    d.district_name,
-    rr.actual_rainfall
-FROM rainfall_record rr
-JOIN district d
-    ON rr.district_id = d.district_id
-WHERE rr.actual_rainfall > 500;
-
 DROP INDEX idx_rainfall_district ON rainfall_record;
 
 SHOW INDEX FROM rainfall_record;
